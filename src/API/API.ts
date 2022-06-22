1238af7a5f2500466fc152bdb173e9a2dcd5238e
OlegJBAc <okarakulinjb@mail.ru>
2022-06-22 11:22:03 +0300

    key

--- src/API/API.ts
+++ src/API/API.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0',
     withCredentials: true,
     headers: {
-        'API-KEY': '904d6c87-d7a6-413c-b2ac-7594fcc363d1'
+        'API-KEY': 'ebf536e3-3303-498b-b5bb-6080ca866d8c'
     }
 })
